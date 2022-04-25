e11efdb90f903c6e81dc04a08f8d1a46ce9006b1
Daniel Kao <daniel.kao@gmail.com>
2022-04-26 00:20:57 +0800

    feat: add recent task button

---
+++ app/src/main/java/info/plateaukao/quickrotate/RecentTaskTileService.kt
@@ -0,0 +1,19 @@
+package info.plateaukao.quickrotate
+
+import android.content.Intent
+import android.service.quicksettings.TileService
+import java.util.*
+
+open class RecentTaskTileService : TileService() {
+    override fun onClick() {
+        val it = Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)
+        baseContext.sendBroadcast(it)
+
+        val timerTaskObj: TimerTask = object : TimerTask() {
+            override fun run() {
+                baseContext.sendBroadcast(Intent("toggle_recent_screen"))
+            }
+        }
+        Timer().schedule(timerTaskObj, 100)
+    }
+}
