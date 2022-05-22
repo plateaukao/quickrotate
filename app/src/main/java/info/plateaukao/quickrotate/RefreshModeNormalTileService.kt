c66b49dec84d0dae5e3c10fb5e711f4ab2dbbbfc
Daniel Kao <daniel.kao@gmail.com>
2022-05-22 21:18:10 +0800

    feat: add separate speed mode buttons

--- app/src/main/java/info/plateaukao/quickrotate/RefreshModeNormalTileService.kt
+++ app/src/main/java/info/plateaukao/quickrotate/RefreshModeNormalTileService.kt
@@ -2,24 +2,16 @@ package info.plateaukao.quickrotate
 
 import android.content.Intent
 import android.service.quicksettings.TileService
-import com.onyx.android.sdk.api.device.epd.EpdController
 import com.onyx.android.sdk.api.device.epd.UpdateOption
 import com.onyx.android.sdk.device.BaseDevice
 import com.onyx.android.sdk.device.Device
-import java.util.*
 
-class RefreshModeTileService : TileService() {
+class RefreshModeNormalTileService : TileService() {
     private val device: BaseDevice = Device.currentDevice()
 
     @Suppress("DEPRECATION")
     override fun onClick() {
         baseContext.sendBroadcast(Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS))
-        val timerTaskObj: TimerTask = object : TimerTask() {
-            override fun run() {
-                device.systemRefreshMode =
-                    if (EpdController.isInFastMode()) UpdateOption.NORMAL else UpdateOption.FAST
-            }
-        }
-        Timer().schedule(timerTaskObj, 100)
+        device.systemRefreshMode = UpdateOption.NORMAL
     }
 }
